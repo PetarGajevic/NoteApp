9ac2d8059f020b4d05ad2c6944a0ad30ff488db3
petar.gajevic <petar.gajevic@amplitudo.me>
2024-03-12 15:31:07 +0100

    Adds Test

---
+++ app/src/androidTest/java/com/example/noteapp/feature_note/presentation/notes/NotesScreenTest.kt
@@ -0,0 +1,14 @@
+package com.example.noteapp.feature_note.presentation.notes
+
+import com.example.noteapp.di.AppModule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Assert.*
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesScreenTest {
+
+
+
+}
